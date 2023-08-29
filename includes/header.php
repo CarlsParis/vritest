60e7b2180497ac0234a7eef5b2a671004a8a99d4
yhpariq <yhpariq@gmail.com>
2023-08-29 17:11:31 -0500

    v4

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=\, initial-scale=1.0">
-    <title>Control Biometrico</title>
+    <title></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/main.css">
 
@@ -12,12 +12,14 @@
 
 <body>
 
-<!-- <div class="container bg-body-tertiary"> -->
+<div class="container-fluid bg-dark">
+
+<div class="container">
 
 
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <nav class="navbar navbar-dark bg-dark navbar-expand-lg tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="index.php">VRA</a>
+            <a class="navbar-brand" href="index.php">Vicerrectorado Academico</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -38,4 +40,5 @@
     </nav>
 
 
-<!-- </div> -->
+</div>
+</div>
--- index.php
+++ index.php
@@ -2,25 +2,36 @@
 <?php include('db/conection.php') ?>
 
 
-<div class="display_none">
+<div class="d-flex justify-conter-center align-items-center" style="min-height: 100vh;">
 
+    <div class="container">
+        <div class="search_container px-5">
+            <div class="container px-5">
+                <div class="container px-5">
+                    <div class="container px-5">
+                        <form class="row g-3" action="db/busqueda.php" method="GET">
+                            <input class="form-control" type="text" min="1" max="8" id="dni" name="query" required autofocus placeholder="Buscar  DNI">
+                            <input class="btn btn-primary" type="submit" value="Buscar">
+                        </form>
+                        <div class="img-center">
+                        <img src="assets/media/vracad_preview.png" alt="" class="img-fluid">
 
-<div class="search_container">
-    <form action="db/busqueda.php" method="GET">
-        <input type="text" min="1" max="8" id="dni" name="query"  required autofocus placeholder="Buscar  DNI">
-        <input type="submit" value="Buscar">
-    </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </div>
-
 <script>
-    var input=  document.getElementById('dni');
-input.addEventListener('input',function(){
-  if (this.value.length > 8) 
-     this.value = this.value.slice(0,8); 
-})
+    var input = document.getElementById('dni');
+    input.addEventListener('input', function() {
+        if (this.value.length > 8)
+            this.value = this.value.slice(0, 8);
+    })
 </script>
 <!-- 
 <input class="input_hiden" type="text" id="codigo" placeholder="Enfoca este input y usa el lector"> -->
 
-</div>
 <?php include('includes/footer.php') ?>
